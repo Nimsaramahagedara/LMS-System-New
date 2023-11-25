71a643efcd2cb183fd2f3c63f1ebb88822d1918a
IT21158704 <it21158704@my.sliit.lk>
2023-11-25 19:19:44 +0530

    TeacherUi

--- client/src/pages/teacher/Attendence.jsx
+++ client/src/pages/teacher/Attendence.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Checkbox from '@mui/material/Checkbox';
+import Avatar from '@mui/material/Avatar';
+import TeacherAttendanceDateCard from '../../components/TeacherAttendanceDateCard';
 
-const Attendence = () => {
+const Attendance = () => {
+  const [checked, setChecked] = React.useState([]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/users');
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Attendence</div>
-  )
-}
+    <div>
+      <TeacherAttendanceDateCard />
+      <List dense sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        {/* Header Row */}
+        <ListItem>
+          <ListItemAvatar>
+            <ListItemText primary="ID" />
+          </ListItemAvatar>
+          <ListItemText primary="Name" />
+        </ListItem>
 
-export default Attendence
+        {/* User Rows */}
+        {users.map((value) => {
+          const labelId = `checkbox-list-secondary-label-${value.id}`;
+          return (
+            <ListItem
+              key={value.id}
+              secondaryAction={
+                <Checkbox
+                  edge="end"
+                  onChange={handleToggle(value.id)}
+                  checked={checked.indexOf(value.id) !== -1}
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              }
+              disablePadding
+            >
+              <ListItemButton>
+                <ListItemAvatar>
+                  <ListItemText id={labelId} primary={`${value.id}`} />
+                </ListItemAvatar>
+                <ListItemText
+                  id={labelId}
+                  primary={`${value.name.firstname} ${value.name.lastname}`}
+                />
+              </ListItemButton>
+            </ListItem>
+          );
+        })}
+      </List>
+    </div>
+  );
+};
+
+export default Attendance;
