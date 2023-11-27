006b1732ca4a6ec9eab2f824cb4b509bbe5c4103
IT21158186 <it21158186@my.sliit.lk>
2023-11-27 21:06:15 +0530

    update

--- client/src/pages/admin/StudentMNG.jsx
+++ client/src/pages/admin/StudentMNG.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@mui/material';
+
+import AdminWelcomeCard from '../../components/AdminWelcomeCard';
+
 
 const StudentMNG = () => {
+  const handleAddStudent = () => {
+    // Add logic to handle adding a new student
+    console.log('Add new student clicked!');
+    // You can add your logic to open a modal, navigate to a new page, or perform any other action.
+  };
+
+
   return (
-    // <div>StudentMNG</div>
-
-    /*Add new Stuident navigation button*/
-    <div className="button-container">
-    {/* <button type="button" className="button" onClick={() => {navigate('/appointments/lab_add');}}>Add new Student</button> */}
+    
+    <div>
+      <AdminWelcomeCard/>
+      <h1>Manage Students</h1>
+    <br></br>
+      <Button variant="contained">Add New Student</Button>
     </div>
+  );
+};
 
-
-
-  )
-}
-
-export default StudentMNG
+export default StudentMNG;
