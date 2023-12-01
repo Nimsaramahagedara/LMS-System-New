5ec2b58ec46d14a222f53e75fbf7eaea8410d46c
= <=>
2023-12-01 20:01:35 +0530

    StudentHomePage

--- client/src/pages/student/StudentDashboard.jsx
+++ client/src/pages/student/StudentDashboard.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../../components/LandingPage/Header'
+import TopNav from '../../components/LandingPage/TopNav'
+import Footer from '../../components/LandingPage/Footer'
+import Cookies from 'js-cookie'
 const StudentDashboard = () => {
-  return (
-    <div>StudentDashboard
-        <Outlet/>
-    </div>
-  )
+    const [isLoggedIn, setLogging] = useState();
+    useEffect(() => {
+        const userRole = Cookies.get('userRole');
+        if(userRole){
+            setLogging(true);
+        }
+    }, [])
+    return (
+        <>
+            <Header isLoggedIn={isLoggedIn}/>
+            <TopNav />
+            <Outlet />
+            <Footer />
+        </>
+    )
 }
 
 export default StudentDashboard
