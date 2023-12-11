7e7f0c7490c0491c61660567cddcb9f39d891aee
IT21158704 <it21158704@my.sliit.lk>
2023-12-11 18:41:49 +0530

    Teacher Contraller

--- server/routes/AdminRoutes.js
+++ server/routes/AdminRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { CreateSupportAccount, deleteSupportMember, getAllSupportMembers, getSupportMember, updateSupportMember } from '../controllers/SupportController.js';
+import { CreateTeacherAccount, deleteTeacher, getAllTeachers, getTeacher, updateTeacher } from '../controllers/TeacherContraller.js';
+
 
 const adminRouter = express.Router();
 
@@ -8,6 +10,12 @@ adminRouter.get('/get-support/:email',getSupportMember);
 adminRouter.put('/update-support/:email',updateSupportMember);
 adminRouter.delete('/delete-support/:email', deleteSupportMember);
 adminRouter.post('/create-support', CreateSupportAccount);
+
+adminRouter.get('/get-all-teachers',getAllTeachers);
+adminRouter.get('/get-teacher/:email',getTeacher);
+adminRouter.put('/update-teacher/:email',updateTeacher);
+adminRouter.delete('/delete-teacher/:email', deleteTeacher);
+adminRouter.post('/create-teacher', CreateTeacherAccount);
 
 
 export default adminRouter;
