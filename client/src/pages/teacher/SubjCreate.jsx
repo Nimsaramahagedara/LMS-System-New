21e333ce4b4c78c6835969288b0e35a421f56b1a
Deneth Pinsara <it21158186@my.sliit.lk>
2023-12-23 20:35:12 +0530

    subj

--- client/src/pages/teacher/SubjCreate.jsx
+++ client/src/pages/teacher/SubjCreate.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
 
-const SubjCreate = () => {
+import { apiUrl } from '../../utils/Constants';
+
+const SubjCreate = ({ classId }) => {
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Check if classId is defined before making the API request
+        if (classId) {
+          const response = await fetch(`${apiUrl}/subjects/${classId}`);
+          const data = await response.json();
+          setSubjects(data);
+        } else {
+          console.error('classId is undefined.');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [classId]);
   return (
-    <div>SubjCreate</div>
-  )
-}
+    <div>
+      <TableContainer component={Paper} style={{ marginTop: '20px' }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>No</TableCell>
+              <TableCell>Subject Name</TableCell>
+              <TableCell>Teacher</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {subjects.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{row.subName}</TableCell>
+                <TableCell>{row.teachBy}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
 
-export default SubjCreate
+export default SubjCreate;
