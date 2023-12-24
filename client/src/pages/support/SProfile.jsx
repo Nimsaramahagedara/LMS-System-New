340e92228ae8da3d06a342fd4f5cf1594d921d05
Deneth Pinsara <it21158186@my.sliit.lk>
2023-12-24 22:16:29 +0530

    support profile view and logout from profile view for teacher and support

--- client/src/pages/support/SProfile.jsx
+++ client/src/pages/support/SProfile.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, Typography, Button, Avatar } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { toast } from 'react-toastify';
+import authAxios from '../../utils/authAxios';
+import { apiUrl } from '../../utils/Constants';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../pages/common/AuthContext';
 
 const SupportProfile = () => {
-  return (
-    <div>My Profile</div>
-  )
-}
+  const [support, setSupport] = useState({
+    firstName: 'Loading',
+    lastName: 'Loading',
+    email: 'Loading',
+    gender: 'Loading',
+    address: 'Loading',
+    // Include other properties as needed
+  });
 
-export default SupportProfile
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  useEffect(() => {
+    const getUserDetails = async () => {
+      try {
+        const response = await authAxios.get(`${apiUrl}/get-user`);
+        setSupport(response.data);
+      } catch (error) {
+        console.error(error);
+        if (error.response && error.response.status === 404) {
+          toast.error('Support profile not found.');
+        } else {
+          toast.error(error.response?.data?.message || 'An error occurred');
+        }
+      }
+    };
+
+    getUserDetails();
+  }, []);
+
+  return (
+    <div className='w-full bg-blue-950 py-4 '>
+      <Card sx={{ padding: '60px 40px', width: 'fit-content', margin: '10px auto' }}>
+        <CardContent>
+          <Avatar sx={{ width: 100, height: 100, margin: '20px auto' }}>
+            <AccountCircleIcon sx={{ width: '100%', height: '100%' }} />
+          </Avatar>
+          <Typography variant="h5" gutterBottom>
+            {`${support.firstName} ${support.lastName}`}
+          </Typography>
+          <Typography>Email: {support.email}</Typography>
+          <Typography>Gender: {support.gender}</Typography>
+          <Typography>Address: {support.address}</Typography>
+
+          <Button size="small" onClick={() => logout()} variant="outlined" sx={{ color: 'black' }}>
+            Log out
+          </Button>
+
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SupportProfile;
--- client/src/pages/teacher/Tprofile.jsx
+++ client/src/pages/teacher/Tprofile.jsx
@@ -1,90 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import { Card, CardContent, Typography, Button, Avatar } from '@mui/material';
-import EditProfileForm from './EditProfileForm';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { toast } from 'react-toastify';
 import authAxios from '../../utils/authAxios';
 import { apiUrl } from '../../utils/Constants';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../pages/common/AuthContext';
 
 
-
-const TeacherProfile = () => {
-
-    const [isEditing, setIsEditing] = useState(false);
-    const [teacher, setTeacher] = useState({
+    const TeacherProfile = () => {
+      const [teacher, setTeacher] = useState({
         firstName: 'Loading',
-    lastName: 'Loading',
-    grade: 'Loading',
-    gender: 'Loading',
-    address: 'Loading',
-    classId: {
-      subClass: 'Loading',
-      grade: 'Loadning'
-    }
+        lastName: 'Loading',
+        email: 'Loading',
+        gender: 'Loading',
+        address: 'Loading',
+        // Include other properties as needed
       });
 
-
-      const handleEditClick = () => {
-        setIsEditing(true);
-      };
+      const navigate = useNavigate();
+      const { logout } = useAuth();
     
-      const handleSave = (updatedProfile) => {
-        // Handle saving the updated profile (e.g., send to the server).
-        console.log('Updated Profile:', updatedProfile);
-        setIsEditing(false);
-      };
-
-
       useEffect(() => {
-        const getUserDetails = async () => {
+
+      const getUserDetails = async () => {
           try {
             const response = await authAxios.get(`${apiUrl}/get-user`);
             setTeacher(response.data);
-            console.log(response.data);
-          } catch (error) {
-            console.log(error);
-            if (error.response && error.response.status === 404) {
-              // Handle 404 error (resource not found)
-              toast.error('Teacher profile not found.');
-            } else {
-              toast.error(error.response?.data?.message || 'An error occurred');
-            }
-          }
-        };
-        getUserDetails();
-      }, []);
-    
-
+              } catch (error) {
+                console.error(error);
+                  if (error.response && error.response.status === 404) {
+                    toast.error('Teacher profile not found.');
+                  } else {
+                    toast.error(error.response?.data?.message || 'An error occurred');
+                  }
+              }
+      };
+    getUserDetails();
+  }, []);
 
   return (
     <div className='w-full bg-blue-950 py-4 '>
-        <Card sx={{ padding: '60px 40px', width: 'fit-content', margin: '10px auto' }}>
-          <CardContent >
-            {isEditing ? (
-              <EditProfileForm teacher={teacher} onSave={handleSave} />
-            ) : (
-              <>
-                {/* Use AccountCircleIcon instead of Avatar */}
-                <Avatar sx={{ width: 100, height: 100, margin: '20px auto' }}>
-                  <AccountCircleIcon sx={{ width: '100%', height: '100%' }} />
-                </Avatar>
-                <Typography variant="h5" gutterBottom>
-                  {`${teacher.firstName} ${teacher.lastName}`}
-                </Typography>
-               
-                <Typography>Email: {teacher.email}</Typography>
-                <Typography>Gender: {teacher.gender}</Typography>
-                <Typography>Address: {teacher.address}</Typography>
-                
-                <Button variant="contained" onClick={handleEditClick} sx={{ marginTop: 2 }}>
-                  Edit Profile
-                </Button>
-              </>
-            )}
-          </CardContent>
-        </Card>
-      </div>
-  )
-}
+      <Card sx={{ padding: '60px 40px', width: 'fit-content', margin: '10px auto' }}>
+        <CardContent>
+           
+              <Avatar sx={{ width: 100, height: 100, margin: '20px auto' }}>
+                <AccountCircleIcon sx={{ width: '100%', height: '100%' }} />
+              </Avatar>
+              <Typography variant="h5" gutterBottom>
+                {`${teacher.firstName} ${teacher.lastName}`}
+              </Typography>
+              <Typography>Email: {teacher.email}</Typography>
+              <Typography>Gender: {teacher.gender}</Typography>
+              <Typography>Address: {teacher.address}</Typography>
+              
+              <Button size="small" onClick={() => logout()} variant="outlined" sx={{ color: 'black' }}>
+                Log out
+              </Button>
+          
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
-export default TeacherProfile
+export default TeacherProfile;
