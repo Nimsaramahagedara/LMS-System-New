98082a2db48b216189b309d8403db546f95127d1
= <=>
2023-12-27 22:43:14 +0530

    vite-config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// /project/client/vite.config.js
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['mongoose'],
+    },
+  },
   plugins: [react()],
-})
-
-
+});
