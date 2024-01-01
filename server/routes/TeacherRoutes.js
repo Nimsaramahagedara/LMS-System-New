db50a55c06934b4109c4b06acd59be7855be4267
IT21158704 <it21158704@my.sliit.lk>
2024-01-01 16:36:36 +0530

    marks

--- server/routes/TeacherRoutes.js
+++ server/routes/TeacherRoutes.js
@@ -3,6 +3,7 @@ import { LoginValidator } from '../middlewares/LoggedIn.js';
 import { getMySubjects, getStudentsInClass, teacherOverview } from '../controllers/TeacherContraller.js';
 import { submitAttendance, getAttendanceByOwnerId, deleteAttendance } from '../controllers/AttendanceController.js';
 
+import { getMarks } from '../controllers/MarksController.js';
 const TeacherRouter = express.Router();
 
 TeacherRouter.use(LoginValidator);
@@ -12,6 +13,7 @@ TeacherRouter.get('/get-students-in-class', getStudentsInClass);
 TeacherRouter.post('/submit-attendance', submitAttendance)
 TeacherRouter.get('/attendance', getAttendanceByOwnerId);
 TeacherRouter.delete('/delete-attendance/:id', deleteAttendance);
+TeacherRouter.get('/marks', getMarks);
 
 
 
