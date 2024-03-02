ef21e9de629f6796d1b5a54e3fc94c8631f18bfb
= <=>
2024-03-02 07:44:25 +0530

    fix

--- client/src/components/LandingPage/TopNav.jsx
+++ client/src/components/LandingPage/TopNav.jsx
@@ -11,38 +11,38 @@ const TopNav = () => {
 
     return (
         <>
-            <div className='items-center lg:px-10 bg-gray-400 hidden  hidden'>
+            <div className='items-center lg:px-10 bg-gray-400 flex '>
                 <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
                     Home
                 </Link>
                 <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
-                    Classes
+                    Vision
+                </Link>
+                <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
+                    Mission
                 </Link>
                 <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
                     Support
-                </Link>
-                <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
-                    Resources
                 </Link>
                 <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
                     Notices
                 </Link>
                 
             </div>
-            <div className={`lg:hidden bg-gray-300 relative hidden`}>
+            <div className={`lg:hidden bg-gray-300 relative block`}>
                 <button className='p-5 ' onClick={handleMenuExpand}><MenuIcon/></button>
                 <div className = {`w-full absolute top-16 left-0 z-10 transition-all ease-in-out duration-300 flex flex-col ${open ? 'visible' : 'invisible'} transform ${open ? 'scale-x-100' : 'scale-x-0'}`}>
                     <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
                         Home
                     </Link>
                     <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
-                        Classes
+                        Vision
+                    </Link>
+                    <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
+                        Mission
                     </Link>
                     <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
                         Support
-                    </Link>
-                    <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
-                        Resources
                     </Link>
                     <Link to={'/'} className='text-white bg-gray-400 text-lg hover:bg-gray-500 p-5 focus:bg-blue-950 focus:text-white'>
                         Notices
