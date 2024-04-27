cb1f29bfe5df35add1f29ea53191997f77658c6c
= <=>
2024-04-27 20:01:21 +0530

    fix

--- client/src/pages/teacher/PublishSubjectMarks.jsx
+++ client/src/pages/teacher/PublishSubjectMarks.jsx
@@ -13,6 +13,8 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '80%',
+    height:'90vh',
+    overflowY:'scroll',
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
